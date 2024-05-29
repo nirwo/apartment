d9958373546fbfcde61da905bac1bc0ac8d29618
nirwo <wolffdesign@gmail.com>
2024-05-29 21:47:52 +0300

    new code

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Apartment Listing App</h1>
+            <p>Welcome to the Apartment Listing App</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ server/index.js
@@ -0,0 +1,30 @@
+// server/index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Apartment Listing API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
